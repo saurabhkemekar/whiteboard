20f8618d01bcf3adb9be2439ec86deff642539df
saurabhkemekar <saurabhkemekar786@gmail.com>
2024-06-18 21:59:28 +0530

    merged

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const cors = require("cors");
+const app = express();
+let port = 3000;
+app.use(cors());
+app.use(express.static("../client"));
+
+let server = app.listen(port, () => {
+  console.log("listening to  port " + port);
+});
+const socket = require("socket.io");
+let io = socket(server, { cors: { origin: "*" } });
+
+// listen for connections
+io.on("connection", (socket) => {
+  console.log("connected");
+
+  socket.on("beginPath", (data) => {
+    io.sockets.emit("beginPath", data);
+  });
+  socket.on("drawPath", (data) => {
+    io.sockets.emit("drawPath", data);
+  });
+  socket.on("undoRedo", (data) => {
+    io.sockets.emit("undoRedo", data);
+  });
+});
